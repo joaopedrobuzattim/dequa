345729aaa1454d7c6c4960bdcd91f8f819ebb042
joaopedrobuzattimendes <joaopedrobuzattimendes@gmail.com>
2021-05-27 08:45:25 -0300

    Vacancy features (vacancies-skills migration)

---
+++ src/shared/infra/Typeorm/migrations/1622115762102-create-vacancies-skills.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class createVacancieskills1622115762102 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'vacancies-skills',
+        columns: [
+          {
+            name: 'skillsId',
+            type: 'uuid',
+          },
+          {
+            name: 'vacanciesId',
+            type: 'uuid',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'FKVacancyId',
+            referencedTableName: 'vacancies',
+            referencedColumnNames: ['id'],
+            columnNames: ['vacanciesId'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'FKSkilld',
+            referencedTableName: 'skills',
+            referencedColumnNames: ['id'],
+            columnNames: ['skillsId'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('vacancies-skills');
+  }
+}
