446ee607ff041372a26204d768d39df221dc76be
João Pedro Buzatti Mendes <71409819+joaopedrobuzattim@users.noreply.github.com>
2021-05-29 23:32:28 -0300

    Change themes restrictTo

--- src/modules/events/infra/http/routes/theme.routes.ts
+++ src/modules/events/infra/http/routes/theme.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import restrictTo from '@shared/infra/http/middlewares/restrictTo';
+
 import ThemesController from '../controllers/ThemesController';
 
 const themesRouter = Router();
 const themesController = new ThemesController();
 
+eventsRouter.use(ensureAuthenticated);
+eventsRouter.use(restrictTo('premiumUser'));
+
 themesRouter.get('/', themesController.list);
 
 export default themesRouter;
+
+
