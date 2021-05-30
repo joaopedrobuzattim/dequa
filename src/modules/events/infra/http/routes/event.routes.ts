a0d91cd149a9952e866246d540ce428c3ee007b8
João Pedro Buzatti Mendes <71409819+joaopedrobuzattim@users.noreply.github.com>
2021-05-29 22:27:51 -0300

    Change events restrictTo

--- src/modules/events/infra/http/routes/event.routes.ts
+++ src/modules/events/infra/http/routes/event.routes.ts
@@ -1,8 +1,13 @@
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import restrictTo from '@shared/infra/http/middlewares/restrictTo';
 import { Router } from 'express';
 import EventsController from '../controllers/EventsController';
 
 const eventsRouter = Router();
 const eventsController = new EventsController();
+
+eventsRouter.use(ensureAuthenticated);
+eventsRouter.use(restrictTo('premiumUser'));
 
 eventsRouter.get('/', eventsController.list);
 eventsRouter.get('/:id', eventsController.getOne);
